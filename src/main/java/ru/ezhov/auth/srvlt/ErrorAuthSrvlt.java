248df1b9fa796d07f5f2bfaea1fbad8064cf1ce9
ezhov-da <ezhdenis@yandex.ru>
2018-12-13 22:08:29 +0300

    Cookie

--- src/main/java/ru/ezhov/auth/srvlt/ErrorAuthSrvlt.java
+++ src/main/java/ru/ezhov/auth/srvlt/ErrorAuthSrvlt.java
@@ -2,6 +2,7 @@ package ru.ezhov.auth.srvlt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,16 @@ import java.io.IOException;
 public class ErrorAuthSrvlt extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                System.out.println(cookie.getName());
+                cookies[i].setValue(null);
+                cookies[i].setMaxAge(0);
+                resp.addCookie(cookie);
+            }
+        }
         resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
